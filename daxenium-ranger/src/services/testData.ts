a7e23b0c83cb47e82a66a53c668f983ddfc93bda
Arva Peter <peter.arva.hu@gmail.com>
2026-06-09 15:12:39 +0200

    Fix testData

--- daxenium-ranger/src/services/testData.ts
+++ daxenium-ranger/src/services/testData.ts
@@ -3,13 +3,24 @@ import { db } from "../database/db";
 export async function createTestLog(
   stockpileId: string
 ) {
+
   await db.logEntries.add({
-  stockpileId: "TT-001",
-  species: "Tölgy",
-  length: 4,
-  diameter: 30,
-  quantity: 1,
-  volume: 0.282,
-  createdAt: new Date().toISOString(),
-});
+
+    stockpileId,
+
+    species: "Tölgy",
+
+    length: 4,
+
+    diameter: 30,
+
+    quantity: 1,
+
+    volume: 0.282,
+
+    createdAt:
+      new Date().toISOString(),
+
+  });
+
 }
